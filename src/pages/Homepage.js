7f874583fa6c7692995ceddb78875062fa11381f
Joseph Musya <josephmusya254@gmail.com>
2023-01-27 17:11:06 +0300

    Added temp and humidity components

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,24 @@
+import styles from './Homepage.module.css'
 function Homepage(){
     return(
-        <div>
-            Homepage
-        </div>     
+        <main>
+            <div className={styles.topDiv}>
+                <div className={styles.temperature}>
+                    <div>
+                        <h2>Temperature</h2>
+                    </div>                    
+                    <hr />
+                    <p>23.6</p>                                        
+                </div>
+                <div className={styles.humidity}>
+                    <div>
+                        <h2>Humidity</h2>
+                    </div> 
+                    <hr />
+                    <p>56</p>
+                </div>
+            </div>                       
+        </main>     
     )
 }
 
